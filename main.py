303c8db897043fb3f5c57a8f87243a06331a3222
adrien-dimitri <amaugueret@gmail.com>
2024-06-24 22:32:42 +0200

    fixed logs path

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ lat = os.getenv('LAT')
 lon = os.getenv('LON')
 api_key = os.getenv('API_KEY')
 
-logging.basicConfig(filename=logname,
+logging.basicConfig(filename='logs.txt',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(message)s',
                     datefmt='%H:%M:%S',
@@ -21,12 +21,10 @@ logging.basicConfig(filename=logname,
 while True:
     temp = get_temperature(lat, lon, api_key)
 
-    print(temp)
-
     if temp is not None:
         if temp > 20:
             toggle_fan(1)
-            logging.info('ON'))
+            logging.info('ON')
         else:
             logging.info('OFF')
             toggle_fan(0)
